2637895f7f0e36b158213630d90667b771104132
AnastasiyaShved <pav159208@gmail.om>
2023-09-21 22:45:16 +0300

    сделан интерфейс

---
+++ HT_22_CatalogWithFeedback/HT_22_CatalogWithFeedback/Models/Feedback.swift
@@ -0,0 +1,14 @@
+//
+//  Feedback.swift
+//  HT_22_CatalogWithFeedback
+//
+//  Created by Apple on 21.09.23.
+//
+
+import Foundation
+
+struct Feedback {
+    private let date = Date()
+    let text: String
+    let mark: Double
+}
---
+++ HT_22_CatalogWithFeedback/HT_22_CatalogWithFeedback/Models/Restaurants.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurants.swift
+//  HT_22_CatalogWithFeedback
+//
+//  Created by Apple on 21.09.23.
+//
+
+import UIKit
+
+struct Restaurants {
+    
+    let name: String
+    let address: String
+    let image: UIImage?
+    
+    var feedBacks: [Feedback] = []
+}
